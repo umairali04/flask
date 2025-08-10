0ff68e7f0415c54c510c99d9f91df67d300b6f8e
Umair Ali <91592871+umairali04@users.noreply.github.com>
2025-08-11 01:00:16 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,16 +6,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-model = joblib.load('heart_disease_model.pkl')
-@app.route('/')
-def home():
-    return jsonify({"message": "API is running"})
+model = joblib.load(__file__.replace("index.py", "heart_disease_model.pkl"))
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
+    data = request.get_json()
     features = np.array([list(data.values())]).astype(float)
     prediction = model.predict(features)[0]
     return jsonify({'prediction': int(prediction)})
-
-if __name__ == '__main__':
-    app.run(debug=True)
