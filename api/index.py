73c5e1ab50124c9891f6b7c9454d61d86b2e006d
Umair Ali <91592871+umairali04@users.noreply.github.com>
2025-08-11 00:52:31 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+model = joblib.load('heart_disease_model.pkl')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    features = np.array([list(data.values())]).astype(float)
+    prediction = model.predict(features)[0]
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
